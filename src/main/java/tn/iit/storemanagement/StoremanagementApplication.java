b39276e4a57445ea4a355996ca930fbd2db2ae9a
Skander <Skander@LAPTOP-PPK2F1IK>
2021-01-07 15:47:51 +0100

    entities

---
+++ src/main/java/tn/iit/storemanagement/StoremanagementApplication.java
@@ -0,0 +1,13 @@
+package tn.iit.storemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoremanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoremanagementApplication.class, args);
+    }
+
+}
